9bf006a677d17e62d1225bc78870a6e2fe329782
programmingwithpython <quasimotoandgirl@gmail.com>
2014-11-18 21:18:26 -0500

    Update dictionaryINPROGRESS.py
    version 2, week 6

--- week5/dictionaryINPROGRESS.py
+++ week5/dictionaryINPROGRESS.py
@@ -1,6 +1,5 @@
 import os
 
-
 def insert():
     word = raw_input("Please Enter A Word:\n")
     definition = raw_input("Please Enter a Definition\n")
@@ -15,6 +14,31 @@ def store(word,definition):
     f.write(','.join([word,definition+'\n']))
     f.close()
 
+def find():
+    queryWord = raw_input("Please Enter a Word To Search For:\n")
+    f = open('default.db','r') # 'r' for read
+    lines = f.readlines()
+    for line in lines:
+        if queryWord in line:
+            word = line.split(',')[0]
+            definition = line.split(',')[1]
+            print "\nword:",word
+            print "definition:",definition
+    raw_input("") # prevents loop from immediately continuing
+    f.close()
+	
+def delete():
+    f = open('default.db','rw')
+    wordToDelete = raw_input("Please Enter a Word To Delete:\n")
+    while True:
+        line = f.readline()
+        if line is None:
+            break
+        else:
+            print line
+
+	        
+    raw_input("") # prevents loop from immediately continuing
 
     
 menuItems = ['Add','Find','Show All','Update','Delete','Quit']
@@ -30,4 +54,6 @@ while True:
     if response in ['1','A','a']:
         insert()
     if response in ['2','L','l']:
-        insert()
+        find()
+    if response in ['5','D','d']:
+        delete()
