ff4ab0683ea9fdc828eaac23f3ce77cc94d72dde
programmingwithpython <quasimotoandgirl@gmail.com>
2014-11-25 11:28:52 -0500

    Update dict_revision.py
    Delete function finalized.  process: read file into memory, filter it with list comprehension in a copied list, write copied list back to file.

--- week7/dict_revision.py
+++ week7/dict_revision.py
@@ -1,22 +1,33 @@
+# always import standard library modules first
 import os
 import re
+
 
 def clearScreen():
     os.system('clear')
 
+# add one word 
 def insert():
+
+    clearScreen()
     word = raw_input("Please Enter A Word:\n")
     definition = raw_input("Please Enter a Definition\n")
     clearScreen()
 
-    print "word: ",word
-    print "definition: ",definition
-    response = raw_input("Does this look okay to you?")
-    if response in ['y','Y','']:
-        store(word,definition)
-    
+    if len(word) < 1:
+        insert()
+
+    else:
+        print "word: ",word
+        print "definition: ",definition
+        response = raw_input("\nDoes this look okay to you?\n")
+        if response in ['y','Y','']:
+            store(word,definition)
+        
+
+# append to the file a comma-delimited string
 def store(word,definition):
-    
+
     # buffer file
     f = open('default.db','a') # a for append
     f.write(','.join([word,definition+'\n']))
@@ -24,6 +35,9 @@ def store(word,definition):
 
 def find():
 
+    clearScreen()
+    
+    # open the file, read into list of lines, close file
     f = open('default.db','r') # 'r' for read
     lines = f.readlines()
     f.close()
@@ -31,66 +45,69 @@ def find():
     # get query from user
     queryWord = raw_input("Please Enter a Word To Search For:\n")
     
+    # split each line by ',' and assign results to word,def
+    # if queryWord found in line, split line into word,def and print
+    clearScreen()
+    print "Search Results:\n"
     for line in lines:
-        if queryWord in line:
-            word = line.split(',')[0]
-            definition = line.split(',')[1]
+        word,definition = line.split(',')
+        if queryWord in word:
             print "\nword:",word
             print "definition:",definition
+
     raw_input("") # prevents loop from immediately continuing
-    f.close()
-    
 
 def showAll():
 
     clearScreen()
-    f=open('default.db','r')
-    lines = f.readlines()
+    print "Dictionary Entries"
+
+    f=open('default.db','r')            # open file for reading
+    lines = f.readlines()               # save lines of file as list
     f.close()
 
-    for line in lines:
-        word,definition = line.split(',')
-        print "\nword:",word
-        print "definition:",definition
-    raw_input("") # prevents loop from immediately continuing
-    f.close()    
+    for line in lines:                      # for each line
+        word,definition = line.split(',')   # split into word,def
+        print "\nword:",word                # print each word,def
+        print "definition:",definition      # 
+    raw_input("")                           # prevents loop from immediately continuing
+
 
 def delete():
-    # read current file into list of lines
-    # delete the lines you don't want using enumerate and 'del' 
-    # close the file, open it as 'w' 
-    # write modified list to it
-    # close file
-    wordToDelete = raw_input("Please Enter a Word To Delete:\n")
-    f = open('default.db','r')
-    lines = f.readlines()
-    for num,val in enumerate(lines):
-        if wordToDelete in val:
-            del lines[num]
-    f.close()
-    f = open('default.db','w')
-    f.writelines(lines)
-    f.flush()
+
+    wordToDelete = raw_input("Please Enter a Word To Delete:\n").strip()
+    f = open('default.db','r')          # open db file
+    lines = f.readlines()               # read lines into temporary list
+    lines_copy = []
     f.close()
 
-    # f = open('default.db','w')
-    # f.writelines(tmpLines)
-    # f.close()
+    # note on the following method:
+    #   deleting a list element while iterating through it isn't a easy task
+    #   because if you are using the index to locate items to delete, 
+    #   that index changes each time you delete an element
+
+    # an easier way is to use a list comprehension like the next python line
+    # it filters lines to give me just the lines without the wordToDelete
+    lines_copy = [line for line in lines if wordToDelete not in line]
 
 
-            
-    raw_input("") # prevents loop from immediately continuing
+    f = open('default.db','w')          # open file again for writing
+    f.writelines(lines_copy)                 # overwrite file with modified list    f.flush()                           # back
+    f.close()                           # close the file
 
-    
-menuItems = ['Add','Find','Show All','Update','Delete','Quit']
+    raw_input("")               # prevents loop from immediately continuing
+
 
 def main():
+
+    menuItems = ['Add','Find','Show All','Update','Delete','Quit']
+    
     while True:
         clearScreen()
         for num, item in enumerate(menuItems):
             print num+1,item
         
-        response = raw_input("Please Choose a Menu Item\n")
+        response = raw_input("\nPlease Choose a Menu Item\n")
         if response in ['6','Q','q']:
             break
         if response in ['1','A','a']:
@@ -101,6 +118,5 @@ def main():
             showAll()
         if response in ['5','D','d']:
             delete()
-
 
 main()
