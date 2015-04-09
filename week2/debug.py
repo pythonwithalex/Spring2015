6d59d11a1e9dbfe50f9147a9940374251ef6ee7f
pythonwithalex <quasimotoandgirl@gmail.com>
2015-04-09 17:27:57 -0400

    Update debug.py

--- week2/debug.py
+++ week2/debug.py
@@ -5,9 +5,14 @@
 
 import random
 
-# notable funcs:
+# notes on the funcs we use:
 #  random.randrange -> gives you a random integer in range(M,N-1)
 #  chr -> turns a number into an ASCII character
+
+
+# Assignment: this function works pretty well, but it isn't perfect, nor very safe
+# 1) see if you can figure out why by testing various input on it
+# 2) finally, add code prevent unexpected errors
 
 def random_string(length):
   random_string = ''
