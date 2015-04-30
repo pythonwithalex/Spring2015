a61eec78693625695882511bd7e0b926a1c42f19
pythonwithalex <quasimotoandgirl@gmail.com>
2015-04-30 16:58:46 -0400

    Create reminder_outline.py

---
+++ week4/reminder/reminder_outline.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Reminder Application
+
+A bare-bones task management application
+
+TODAY
+
+1. set up an sqlite3 database 
+2. get user's task information 
+3. convert the user's date and time information into a datetime object
+4. commit our formatted user input to the database
+5. get the <N> most recent tasks 
+
+NEXT WEEK:
+
+1. modify our program so it takes command line arguments 
+
+
+'''
+
+# imports
+
+# set up database connection
+
+# create db table if not already existing
+
+def getNewEvent():
+  pass
+
+def commitNewEvent(event):
+  pass
+  
+def getNMostRecent(N=4):
+  pass
+
+def getUpcomingEvents(N=4):
+  pass
+
+def mainLoop():
+  pass
+
+mainLoop()
