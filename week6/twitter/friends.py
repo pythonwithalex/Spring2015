902ece72bc7f0bd7d6dac95b6b2f0ffcce28af13
pythonwithalex <quasimotoandgirl@gmail.com>
2015-05-14 16:24:37 -0400

    Update friends.py

--- week6/twitter/friends.py
+++ week6/twitter/friends.py
@@ -2,10 +2,10 @@
 
 import tweepy
 
-consumer_key = 'ozimqHNo7fPpSrjwE0tBbYdXn'
-consumer_secret = 'OD4ucwCNZ8RCOAHGwfsaEHi1uko9tOapYcF08j8CYN3aMJoY8x'
-access_token = '3115539945-umtdlTQzeaIzXa7zg6Ujmu3maIzt2NQkzSpb5fA'
-access_token_secret = 'XWEZGdFU10VEZUG9qi0xvgSJ7dpxyvHiilN54A1q5M1oc'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
