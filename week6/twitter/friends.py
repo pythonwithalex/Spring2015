cf8606139b252d3116e26d601b9e724b346e6888
pythonwithalex <quasimotoandgirl@gmail.com>
2015-05-14 16:01:42 -0400

    Update friends.py

--- week6/twitter/friends.py
+++ week6/twitter/friends.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python
 
+import tweepy
+
+consumer_key = 'ozimqHNo7fPpSrjwE0tBbYdXn'
+consumer_secret = 'OD4ucwCNZ8RCOAHGwfsaEHi1uko9tOapYcF08j8CYN3aMJoY8x'
+access_token = '3115539945-umtdlTQzeaIzXa7zg6Ujmu3maIzt2NQkzSpb5fA'
+access_token_secret = 'XWEZGdFU10VEZUG9qi0xvgSJ7dpxyvHiilN54A1q5M1oc'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+for friend in tweepy.Cursor(api.friends).items():
+    # Process the friend here
+    print(friend.name)
