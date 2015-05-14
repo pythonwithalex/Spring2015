ec9e017663503a1b71660a3028abb9530fd1982a
pythonwithalex <quasimotoandgirl@gmail.com>
2015-05-14 16:44:06 -0400

    Update friends.py

--- week6/twitter/friends.py
+++ week6/twitter/friends.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Note: If you get an InsecurePlatformWarning, you can run 'python -W ignore <python file>' to silence them temporarily.
+# WARNING: he he ... the warnings are there for your benefit, so don't ignore them in your normal development context.
+
 import tweepy
 
 consumer_key = ''
@@ -12,5 +15,12 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
+friend_objs = [ friend for friend in tweepy.Cursor(api.friends).items() ]
+
+# The list comprehension above is equivalent to :
+# for friend in tweepy.Cursor(api.friends).items():
+#     print friend
+
+print 'you have {} friends on twitter'.format(len(friend_objs))
 for friend in tweepy.Cursor(api.friends).items():
     print(friend.name)
