8b88ab61fb19b5c27fc24b33a43cb2627512435e
pythonwithalex <quasimotoandgirl@gmail.com>
2015-05-14 19:21:07 -0400

    Update last_weeks_assignment.py

--- week6/last_weeks_assignment.py
+++ week6/last_weeks_assignment.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python
 
+words = [
+	'representative', 
+	'numeric', 
+	'senile', 
+	'effluent', 
+	'ear', 
+	'bespectacled' , 
+	'elegiac',
+	'ex-developer' 
+	]
+
+def count_e(s):
+  return s.count('e')
+  
+sorted_words = sorted(words,key=count_e,reverse=True)
+
+for word in sorted_words:
+  print word, word.count('e')
