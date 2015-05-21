dca4b792e684e13484b5ff0a73ff44a8bd7f9da0
pythonwithalex <quasimotoandgirl@gmail.com>
2015-05-21 10:51:51 -0400

    Update json_twitter.py

--- week7/json_twitter.py
+++ week7/json_twitter.py
@@ -39,8 +39,30 @@ def is_valid(word):
         rv = False
     if len([c for c in word if c.isalpha()]) < (len(word)/2):
         rv = False
-    if 'RT\t@' in word:
+    if 'RT @' in word:
         rv = False
     if 'http://' in word:
         rv = False
     return rv
+
+def sort_by_count(two_tuple):
+    return two_tuple[1]
+
+words = [ pare(word.encode('utf-8')) 
+            for line in tweets_text
+            for word in line.split() if is_valid(word) ]
+
+#print len(words)
+
+words_set = set(words)
+
+data = []
+
+for word in words_set:
+    data.append((word, words.count(word)))
+    #print word,':',words.count(word)
+    
+words_desc = sorted(data,key=sort_by_count,reverse=True)
+for word in words_desc:
+    print word[0],':',word[1]
+    
